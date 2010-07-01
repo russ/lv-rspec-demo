b4f6d9b49d796cdd7ac0b046431bd6333255f779
Russ Smith <russ@bashme.org>
2010-07-01 14:58:54 -0700

    Full specs.

--- lib/models/credit_card.rb
+++ lib/models/credit_card.rb
@@ -1,10 +1,29 @@
 class CreditCard
   include ActiveModel::Validations
 
+  validates_presence_of :first_name, :last_name
+  validates_presence_of :number, :expiration_month, :expiration_year, :cvv
+  validate :validate_number
+
   attr_accessor :first_name, :last_name
   attr_accessor :number, :expiration_month, :expiration_year, :cvv
 
+  def initialize(attributes = {})
+    @first_name = attributes[:first_name]
+    @last_name = attributes[:last_name]
+    @number = attributes[:number]
+    @expiration_month = attributes[:expiration_month]
+    @expiration_year = attributes[:expiration_year]
+    @cvv = attributes[:cvv]
+  end
+
 private
+
+  def validate_number
+    unless valid_card_number_length?(@number) && valid_checksum?(@number)
+      errors.add(:number, 'number is invalid')
+    end
+  end
 
   def valid_card_number_length?(number)
     number.to_s.length >= 12
--- spec/models/credit_card_spec.rb
+++ spec/models/credit_card_spec.rb
@@ -1,14 +1,61 @@
 require 'spec_helper'
 
 describe CreditCard do
+  before(:each) do
+    @credit_card = credit_card
+  end
+
   describe "validations" do
-    it "should require :first_name"
-    it "should require :last_name"
-    it "should require :number"
-    it "should require :expiration_month"
-    it "should require :expiration_year"
-    it "should require :cvv"
-    it "should not validate invalid card numbers"
-    it "should validate valid card numbers"
+    it "should require :first_name" do
+      @credit_card.first_name = ''
+      @credit_card.valid?.should == false
+    end
+
+    it "should require :last_name" do
+      @credit_card.last_name = ''
+      @credit_card.valid?.should == false
+    end
+
+    it "should require :number" do
+      @credit_card.number = ''
+      @credit_card.valid?.should == false
+    end
+
+    it "should require :expiration_month" do
+      @credit_card.expiration_month = ''
+      @credit_card.valid?.should == false
+    end
+
+    it "should require :expiration_year" do
+      @credit_card.expiration_year = ''
+      @credit_card.valid?.should == false
+    end
+
+    it "should require :cvv" do
+      @credit_card.cvv = ''
+      @credit_card.valid?.should == false
+    end
+
+    it "should not validate invalid card numbers" do
+      @credit_card.number = ''
+      @credit_card.valid?.should == false
+
+      @credit_card.number = '41111111111ff'
+      @credit_card.valid?.should == false
+    end
+
+    it "should validate valid card numbers" do
+      @credit_card.number = '4111111111111111'
+      @credit_card.valid?.should == true
+    end
+  end
+
+  def credit_card(overrides = {})
+    CreditCard.new({ :first_name => 'Longbob',
+      :last_name => 'Thompson',
+      :number => '4111111111111111',
+      :expiration_month => 1,
+      :expiration_year => 2020,
+      :cvv => 477 }.merge(overrides))
   end
 end
